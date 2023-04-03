3e9eec2f0a19734aea5beae96b7082767ee0c42b
Mahdi Moazeni <mahdi.moazeni.artisan@gmail.com>
2023-04-04 03:45:18 +0430

    modified route inertia render Home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    /*return view('welcome');
+    return inertia('Home');*/
+    return Inertia::render('Home');
 });
