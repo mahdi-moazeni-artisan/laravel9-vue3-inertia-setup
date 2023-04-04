8fc25eb5028652a345f49e4e991a369c0b9f7184
Mahdi Moazeni <mahdi.moazeni.artisan@gmail.com>
2023-04-04 10:12:26 +0430

    modified Home.vue and web.php

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,45 @@
 <template>
-    <h1>Home Page!</h1>
+      <h1>Home Page!</h1>
+      <h2>{{message}}</h2>
+      <h2>{{props.title}}</h2>
+      <p>
+        <button @click="decrement">-</button>
+        {{counter}}
+        <button @click="increment">+</button>
+      </p>
+      <hr/>
+      <p>
+        Linkedin:mahdi-moazeni-artisan
+      </p>
+      <p>
+        Github:mahdi-moazeni-artisan
+      </p>
+      <p>
+        StackOverflow:https://stackoverflow.com/users/21455398/mahdi-moazeni-artisan
+      </p>
+      <p>
+        Email:mahdi.moazeni.artisan@gmail.com
+      </p>
+      <p>
+        Website:https://idea-processors.ir
+      </p>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+    import { defineProps } from 'vue';
 
+    const message="Welcome to my SPA!";
+    const counter=ref(0);
+    function increment(){
+        counter.value++;
+    }
+    function decrement(){
+        counter.value--;
+    }
+    const props=defineProps(['title']);
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     /*return view('welcome');
-    return inertia('Home');*/
-    return Inertia::render('Home');
+    return inertia('Home');
+    return Inertia::render('Home');*/
+    return Inertia::render('Home',['title'=>'Laravel9+Vue3+Inertia Setup']);
 });
